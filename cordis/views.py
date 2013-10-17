73e8dfcc68b24efd0216ee214658d6acf46e2ea1
Peter Vanhee <peter.vanhee@gmail.com>
2013-10-17 19:34:57 +0200

    Fix API calls

--- cordis/views.py
+++ cordis/views.py
@@ -1,22 +1,10 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from cordis.models import Project
 from cordis.serializers import ProjectSerializer
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-# enable if doing POST
-# @csrf_exempt
-
+@api_view(['GET'])
 def project_list(request):
     """
     List all existing projects
@@ -24,45 +12,24 @@ def project_list(request):
     if request.method == 'GET':
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
-    # elif request.method == 'POST':
-    #     data = JSONParser().parse(request)
-    #     serializer = SnippetSerializer(data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return JSONResponse(serializer.data, status=201)
-    #     else:
-    #         return JSONResponse(serializer.errors, status=400)
-
-
+@api_view(['GET'])
 def project_detail(request, pk):
     """
-    Retrieve a project, and parse from Cordis if not yet available
+    Retrieves project data, or parses the project data from <a href="http://cordis.europa.eu" target="_blank">Cordis</a> if not yet available in the local data storage.
     """
     try:
         project = Project.objects.get(pk=pk)
     except Project.DoesNotExist:
-
-    	# Parse on the fly now
-    	project = Project(rcn=pk)
-    	project.parse_cordis()
-    	project.save()
-        # return HttpResponse(status=404)
+    	try:
+			# Parse on the fly now
+			project = Project(rcn=pk)
+			project.parse_cordis()
+			project.save()
+    	except:
+        	return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ProjectSerializer(project)
-        return JSONResponse(serializer.data)
-
-    # elif request.method == 'PUT':
-    #     data = JSONParser().parse(request)
-    #     serializer = SnippetSerializer(snippet, data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return JSONResponse(serializer.data)
-    #     else:
-    #         return JSONResponse(serializer.errors, status=400)
-
-    # elif request.method == 'DELETE':
-    #     snippet.delete()
-    #     return HttpResponse(status=204)
+        return Response(serializer.data)
