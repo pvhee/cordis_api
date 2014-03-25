1fc09b19d45985859a6837cafffab3091b8bf4ea
Peter Vanhee <peter.vanhee@gmail.com>
2014-03-25 10:20:41 +0100

    fixed settings

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -237,7 +237,7 @@ except ImportError:
             # The following settings are not used with sqlite3:
             'USER': 'cordis',
             'PASSWORD': 'cordis',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
         }
     }
