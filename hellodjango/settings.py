375cf888d8938ea34db362f437a740b08872f281
Peter Vanhee <peter.vanhee@gmail.com>
2014-03-25 10:33:49 +0100

    using sqllite

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -231,14 +231,18 @@ except ImportError:
     
 
     DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #     'NAME': 'cordis',                 # Or path to database file if using sqlite3.
+        #     # The following settings are not used with sqlite3:
+        #     'USER': 'cordis',
+        #     'PASSWORD': 'cordis',
+        #     'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #     'PORT': '',                      # Set to empty string for default.
+        # }
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'cordis',                 # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'cordis',
-            'PASSWORD': 'cordis',
-            'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': '/tmp/cordis'                 # Or path to database file if using sqlite3.
         }
     }
 
